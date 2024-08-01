126adea4fc87a87c585c8210980cab4675dab53a
Rashmi <165359201+rashmibarodiya@users.noreply.github.com>
2024-08-01 08:17:19 +0000

    updated

---
+++ my-app/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+// import NextAuth from "next-auth/next";                  
+// import GoogleProvider from "next-auth/providers/google";
+
+
+// const handler = NextAuth ({
+//     providers : [
+//         GoogleProvider({
+//             clientId :fjkjdhf,
+//             clientSecret : fkhdkfhk
+//         })
+//     ]
+// })
+
+// export {handler as GET , handler as POST}
+
+// console.log("hey there " +process.env.HI??"")
